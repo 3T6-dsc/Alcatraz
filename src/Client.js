4cbd55a71b2f715adef5b2ba250996edc69224a1
3t6 <110821381+3T6-dsc@users.noreply.github.com>
2025-12-26 17:57:12 +0100

    v

--- src/Client.js
+++ src/Client.js
@@ -1,4 +1,3 @@
-
 //  ______   __                      __                                  
 // /      \ |  \                    |  \                                 
 //|  $$$$$$\| $$  _______  ______  _| $$_     ______   ______   ________ 
@@ -14,13 +13,13 @@
 //● Github: https://github.com/GalackQSM/Alcatraz                                                  
 //=======================================================================                                                                      
                                                                        
-const { Client, Collection, EmbedBuilder } = require('discord.js');
+const Discord = require('discord.js');
 const { readdir, readdirSync } = require('fs');
 const { join, resolve } = require('path');
 const AsciiTable = require('ascii-table');
 const { fail } = require('./utils/emojis.json');
 
-class AlcatrazClient extends Client {
+class Client extends Discord.Client {
  constructor(config, options = {}) {  
     super(options);
     this.logger = require('./utils/logger.js');
@@ -42,14 +41,18 @@ class AlcatrazClient extends Client {
       ADMIN: 'Administration',
       OWNER: 'Propriétaire'
     };
-    this.commands = new Collection();
-    this.aliases = new Collection();
+    this.commands = new Discord.Collection();
+    this.aliases = new Discord.Collection();
+    this.topics = [];
     this.token = config.token;
     this.apiKeys = config.apiKeys;
     this.ownerId = config.ownerId;
     this.bugReportChannelId = config.bugReportChannelId;
+    this.feedbackChannelId = config.feedbackChannelId;
+    this.serverLogId = config.serverLogId;
     this.utils = require('./utils/utils.js');
-    this.logger.info('Initialisation du client Alcatraz v14...');
+    this.logger.info('Initialisation...');
+
   }
 
   loadEvents(path) {
@@ -61,8 +64,9 @@ class AlcatrazClient extends Client {
       files.forEach(f => {
         const eventName = f.substring(0, f.indexOf('.'));
         const event = require(resolve(__basedir, join(path, f)));
-        this.on(eventName, event.bind(null, this));
-        this.logger.info(`Chargement de l'événement: ${eventName}`);
+        super.on(eventName, event.bind(null, this));
+        delete require.cache[require.resolve(resolve(__basedir, join(path, f)))]; 
+        this.logger.info(`Chargement de l'évenement: ${eventName}`);
       });
     });
     return this;
@@ -72,7 +76,7 @@ class AlcatrazClient extends Client {
     this.logger.info('Chargement des commandes...');
     let table = new AsciiTable('Commandes');
     table.setHeading('Fichiers', 'Aliases', 'Catégories', 'Statut');
-    readdirSync(path).filter(f => !f.endsWith('.js')).forEach(dir => {
+    readdirSync(path).filter( f => !f.endsWith('.js')).forEach( dir => {
       const commands = readdirSync(resolve(__basedir, join(path, dir))).filter(f => f.endsWith('js'));
       commands.forEach(f => {
         const Command = require(resolve(__basedir, join(path, dir, f)));
@@ -88,7 +92,9 @@ class AlcatrazClient extends Client {
           }
           table.addRow(f, aliases, command.type, 'pass');
         } else {
+          this.logger.warn(`${f} échec du chargement`);
           table.addRow(f, '', '', 'fail');
+          return;
         }
       });
     });
@@ -97,23 +103,29 @@ class AlcatrazClient extends Client {
   }
 
   isOwner(user) {
-    return user.id === this.ownerId;
+    if (user.id === this.ownerId) return true;
+    else return false;
   }
 
   sendSystemErrorMessage(guild, error, errorMessage) {
+
     const systemChannelId = this.db.settings.selectSystemChannelId.pluck().get(guild.id);
     const systemChannel = guild.channels.cache.get(systemChannelId);
 
-    if (!systemChannel || !systemChannel.viewable) return;
+    if ( 
+      !systemChannel || 
+      !systemChannel.viewable || 
+      !systemChannel.permissionsFor(guild.me).has(['SEND_MESSAGES', 'EMBED_LINKS'])
+    ) return;
 
-    const embed = new EmbedBuilder()
-      .setAuthor({ name: this.user.tag, iconURL: this.user.displayAvatarURL() })
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(`${this.user.tag}`, this.user.displayAvatarURL({ dynamic: true }))
       .setTitle(`${fail} Erreur système: \`${error}\``)
       .setDescription(`\`\`\`diff\n- Défaillance du système\n+ ${errorMessage}\`\`\``)
       .setTimestamp()
-      .setColor('#FF0000');
-    systemChannel.send({ embeds: [embed] });
+      .setColor(guild.me.displayHexColor);
+    systemChannel.send(embed);
   }
 }
 
-module.exports = AlcatrazClient;
+module.exports = Client;
